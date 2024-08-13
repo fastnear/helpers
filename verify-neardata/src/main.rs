188e29aacd180c0dd0516e4c85a3fc174b5b5706
Eugene The Dream <ek@remote>
2024-08-13 14:47:54 -0700

    Verify repeated data_id

--- verify-neardata/src/main.rs
+++ verify-neardata/src/main.rs
@@ -1,8 +1,10 @@
 use dotenv::dotenv;
 use fastnear_neardata_fetcher::fetcher::{fetch_block_until_success, DEFAULT_TIMEOUT};
 use fastnear_primitives::block_with_tx_hash::BlockWithTxHashes;
+use fastnear_primitives::near_indexer_primitives::views::ReceiptEnumView;
 use fastnear_primitives::near_primitives::types::BlockHeight;
 use reqwest::Client;
+use std::collections::HashMap;
 use std::env;
 use std::time::Duration;
 
@@ -62,6 +64,7 @@ async fn main() {
         .header
         .height;
 
+    let mut data_ids = HashMap::new();
     let mut prev_block_hash = None;
     for block_height in starting_block_height..=last_block_height {
         let block = fetch_block_by_height(&client, block_height, DEFAULT_TIMEOUT).await;
@@ -86,6 +89,25 @@ async fn main() {
             );
         }
         for shard in block.shards {
+            if let Some(chunk) = shard.chunk {
+                for receipt in chunk.receipts {
+                    match receipt.receipt {
+                        ReceiptEnumView::Action { .. } => {
+                            // skipping here, since we'll get one with execution
+                        }
+                        ReceiptEnumView::Data { data_id, .. } => {
+                            if let Some(other_id) =
+                                data_ids.insert(data_id, receipt.receipt_id.clone())
+                            {
+                                panic!(
+                                    "Data id {} is already present at block height {}. Receipt ids: {} and {}",
+                                    data_id, block_height, receipt.receipt_id, other_id
+                                );
+                            }
+                        }
+                    }
+                }
+            }
             for outcome in shard.receipt_execution_outcomes {
                 if outcome.tx_hash.is_none() {
                     panic!(
